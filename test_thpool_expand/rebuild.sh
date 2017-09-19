e7721bb984c2243bc82970ea58b29987f75a23fa
Dong Liang <liangdong_xjtu@163.com>
2017-09-19 16:56:20 +0800

    Add test_thpool_expand

---
+++ test_thpool_expand/rebuild.sh
@@ -0,0 +1,1 @@
+gcc  test_thpool_expand.c ../thpool.c -D THPOOL_DEBUG -pthread -o test_thpool_expand
---
+++ test_thpool_expand/test_thpool_expand.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include "../thpool.h"
+
+int max_print = 0;
+
+void* print(void* argp) {
+    int num = *((int*)argp);
+    if ( max_print < num )
+        max_print = num;
+    int i=0;
+    while(i<10) {
+        printf("%d.", num);
+        usleep(500*1000);
+        fflush(stdout);
+        i++;
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    char* p;
+    if (argc != 3){
+        puts("This testfile needs excactly two arguments");
+        exit(1);
+    }
+    int num_jobs    = strtol(argv[1], &p, 10);
+    int num_threads = strtol(argv[2], &p, 10);
+    int np[num_jobs];
+
+    threadpool thpool = thpool_init(num_threads);
+
+    int n;
+    for (n=0; n<num_jobs; n++){
+        np[n] = n;
+        thpool_add_work(thpool, (void*)print, (void*)(&np[n]));
+    }
+    thpool_wait(thpool);
+    printf("\nDone: max_print = %d\n", max_print);
+
+    return 0;
+}
